cda31436c197a20d127438f6484c73aac32fcf13
qdonnellan <qdonnellan@gmail.com>
2014-03-23 10:33:23 -0500

    changed the doctstring statement to appear more descriptive

--- tests/exponential_test.py
+++ tests/exponential_test.py
@@ -8,18 +8,18 @@ class TestExponentialFunctionImplementation(unittest.TestCase):
 
     def test_case_of_3_to_the_0(self):
         """
-        3**0 should be 1
+        exponential(3,0) should be 1
         """
         self.assertEqual(exponential(3,0), 1)
 
     def test_case_of_2_to_the_3(self):
         """
-        2**3 should be 8
+        exponential(2,3) should be 8
         """
         self.assertEqual(exponential(2,3), 8)
 
     def test_case_if_negative_exponent(self):
         """
-        2^(-9) should raise a value error
+        exponential(2,-9) should raise a value error
         """
         self.assertRaises(ValueError, exponential, 2,-9)
