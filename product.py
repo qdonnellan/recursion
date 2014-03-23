a26d9165a7107ead847e8528d53d11bbb7505247
qdonnellan <qdonnellan@gmail.com>
2014-03-23 10:53:47 -0500

    can now handle negative multiplication, still only using + and - operators

--- product.py
+++ product.py
@@ -3,7 +3,14 @@ def product(a, b):
     return the product of a and b using only + and - operators
     """
     if type(b) is not int:
-        raise TypeError('second argument must be an integer')
+        raise TypeError("second argument must be an integer")
+
+    if b < 0:
+        # if b is negative,
+        # change the sign of a, change the sign of b
+        # using only the - operator
+        a = a - a - a
+        b = b - b - b
 
     if b == 1:
         return a
